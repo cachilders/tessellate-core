b9c936c0cfc8490a969f39984640e95e5cac21e8
Casey Childers <cachilders@gmail.com>
2017-11-16 22:44:01 -0800

    [refactor] update configs

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const db = require('./config/db');
+const db = require('./db');
 
 app.use(bodyParser.json());
 
